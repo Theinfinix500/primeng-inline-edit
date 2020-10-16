1a7a5351329eafa586e6d36a504e945440afb8f3
abdelali djebara <jbaraali18@gmail.com>
2020-10-16 18:55:22 +0100

    inline edit and responsive grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,132 @@
-import { Component } from '@angular/core';
+import { ProductService } from './services/products.service';
+import { Product } from './models/Product';
+import { Component, ViewChild } from '@angular/core';
+import { MessageService, SelectItem } from 'primeng/api';
+import { Table } from 'primeng/table';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService],
 })
 export class AppComponent {
-  title = 'inline-edit-primeng';
+  @ViewChild('table') table: Table;
+  productForm: FormGroup;
+
+  products1: Product[];
+
+  products2: Product[];
+
+  statuses: SelectItem[];
+
+  clonedProducts: { [s: string]: Product } = {};
+  editingRowKeys: { [s: string]: boolean } = {};
+
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private fb: FormBuilder
+  ) {}
+
+  get tableRowArray(): FormArray {
+    return this.productForm.get('tableRowArray') as FormArray;
+  }
+
+  ngOnInit() {
+    this.createForm();
+    this.productService
+      .getProductsSmall()
+      .subscribe((data) => (this.products1 = data));
+    this.productService.getProductsSmall().subscribe((data) => {
+      let editingRowKeys = { '0': true };
+      this.products2 = data;
+      this.dataToFormGroups(data);
+      data.forEach((item) => (editingRowKeys[item.id] = true));
+      this.editingRowKeys = editingRowKeys;
+      console.log(editingRowKeys);
+    });
+
+    this.statuses = [
+      { label: 'In Stock', value: 'INSTOCK' },
+      { label: 'Low Stock', value: 'LOWSTOCK' },
+      { label: 'Out of Stock', value: 'OUTOFSTOCK' },
+    ];
+  }
+
+  createForm() {
+    this.productForm = this.fb.group({
+      tableRowArray: this.fb.array([]),
+    });
+  }
+
+  createTableRow(row: Product = null) {
+    if (row) {
+      return this.fb.group({
+        id: this.fb.control(row.id),
+        code: this.fb.control(row.code),
+        name: this.fb.control(row.name),
+        category: this.fb.control(row.category),
+        quantity: this.fb.control(row.quantity),
+      });
+    }
+  }
+
+  dataToFormGroups(data: Array<Product>) {
+    // this.productForm
+    //   .get('tableRowArray')
+    //   .setValue();
+
+    data.forEach((item) => this.tableRowArray.push(this.createTableRow(item)));
+    console.log(this.productForm);
+  }
+
+  addNewRow() {
+    // this.tableRowArray.push(this.createTableRow());
+    let group: FormGroup = this.fb.group({
+      id: this.fb.control('0'),
+      code: this.fb.control('fes23234'),
+      name: this.fb.control('abdelali'),
+      category: this.fb.control('fdfgfdg'),
+      quantity: this.fb.control('333'),
+    });
+
+    this.tableRowArray.insert(0, group);
+  }
+
+  saveData() {
+    console.log(this.productForm.get('tableRowArray').value);
+  }
+
+  onDeleteRow(index: number) {
+    this.tableRowArray.removeAt(index);
+  }
+
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id] = { ...product };
+  }
+
+  onRowEditSave(product: Product) {
+    if (product.price > 0) {
+      console.log(this.table.value);
+      delete this.clonedProducts[product.id];
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Product is updated',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Invalid Price',
+      });
+    }
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products2[index] = this.clonedProducts[product.id];
+    delete this.products2[product.id];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule,
+    TableModule,
+    ToastModule,
+    CalendarModule,
+    SliderModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DialogModule,
+    ButtonModule,
+    DropdownModule,
+    ProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
